da13cf8aa60a5fee25b53ddbd4f7bb6a8c193b2b
Davy Guilherme <dvybatista@gmail.com>
2024-05-22 13:42:55 -0300

    feat: Auth Context initial

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -10,10 +10,12 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { DarkModeContext } from '../../context/darkModeContext';
+import { AuthContext } from '../../context/authContext';
 
 const NabBar = () => {
 
     const {toggle, darkMode} = useContext(DarkModeContext);
+    const {currentUser} = useContext(AuthContext);
 
     return (
         <div className='navbar'>
@@ -35,8 +37,8 @@ const NabBar = () => {
                 <EmailOutlinedIcon />
                 <NotificationsOutlinedIcon />
                 <div className="user">
-                    <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="#" />
-                    <span>Davy Guilherme</span>
+                    <img src={currentUser.profilePic} alt="#" />
+                    <span>{currentUser.name}</span>
                 </div>
             </div>
         </div>
