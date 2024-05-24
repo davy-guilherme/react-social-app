aeeec0a6b4a7025bffbe805a4d6062b51cb5ed08
Davy Guilherme <dvybatista@gmail.com>
2024-05-24 16:59:53 -0300

    feat: Login API Connection

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 export const AuthContext = createContext();
 
@@ -6,14 +7,29 @@ export const AuthContextProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(
         JSON.parse(localStorage.getItem("user")) || null
     );
+    
 
-    const login = () => {
+    const login = async (inputs) => {
         // TODO
-        setCurrentUser({
-            id: 1, 
-            name: 'Davy Guilherme', 
-            profilePic: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+        // setCurrentUser({
+        //     id: 1, 
+        //     name: 'Davy Guilherme', 
+        //     profilePic: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+        // })
+
+        const res = await axios.post("http://localhost:8800/api/auth/login", inputs, {
+            withCredentials: true, // inportant to work with cookies
         })
+
+        // console.log('aqui')
+        
+        // setCurrentUser(res.data)
+
+        if (res && res.data) {
+            setCurrentUser(res.data);
+        } else {
+            console.error('Resposta inesperada:', res);
+        }
     }
 
     useEffect(() => {
